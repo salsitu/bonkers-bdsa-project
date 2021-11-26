624ad32de6f47fa8498645720df22cc74136ba46
Frederik Svendsen <frsv@itu.dk>
2021-11-26 18:48:42 +0100

    Changed NavMenu

--- ProjectBank/Shared/Project.cs
+++ ProjectBank/Shared/Project.cs
@@ -1,10 +1,25 @@
 namespace ProjectBank.Shared;
 
-public class Project
-{
-   
-    public string Title { get; set; }
+    public class Project
+    {
+    public int id { get; set; }
+    public string name { get; set; }
+    public string desc { get; set; }
+    public string author { get; set; }
+    //public ICollection<int> applications { get; set; }
+    public int nrOfViews { get; set; }
+    public float ratio { get; set; }
 
-  
-    public string Description { get; set; }
+    public Project() {}
+
+    public Project(int Id, string name, string desc, string author, int nrOfViews, float ratio)
+    {
+        this.id = Id;
+        this.name = name;
+        this.desc = desc;
+        this.author = author;
+        this.nrOfViews = nrOfViews;
+        this.ratio = ratio;
+
+    }
 }

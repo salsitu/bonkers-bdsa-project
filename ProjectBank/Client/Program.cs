5ba4f464593f2c7457a4bf04c90b2a0bb44026c4
Frederik Svendsen <frsv@itu.dk>
2021-12-01 13:58:37 +0100

    Fixed bug

--- ProjectBank/Client/Program.cs
+++ ProjectBank/Client/Program.cs
@@ -11,8 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 /*builder.Services.AddHttpClient("ProjectBank.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 */
-//ADDED THIS INSTEAD
- builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ProjectBank.ServerAPI"));
@@ -23,6 +21,7 @@ builder.Services.AddMsalAuthentication(options =>
     options.ProviderOptions.DefaultAccessTokenScopes.Add("api://612e7abd-16fb-416b-8b5d-9e1b9e665017/API.Access");
 });
 
-
+//ADDED THIS INSTEAD
+ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();

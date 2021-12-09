e108e1b0ab47577ef09ee2be18ff4a9fab048f42
salsitu <90044151+salsitu@users.noreply.github.com>
2021-12-09 21:46:15 +0100

    Fix scripts (#12)
    * Fix scripts.
    
    * Fix crash when SQL server takes a bit long to fully start.
    
    * Add shebang to startup scripts.

--- run.ps1
+++ run.ps1
@@ -1,21 +1,25 @@
+#!/usr/bin/env pwsh
+
 $project = "./Server"
 $entities = "./Server.Entities"
 $database = "sql_server"
 $sqlPass = New-Guid
-$connectionString = "Server=localhost;Database=$database;User Id=sa;Password=$sqlPass;Trusted_Connection=False;Encrypt=False"
+$connectionString = "Server=localhost;Database=$database;User Id=sa;Password=$sqlPass;Trusted_Connection=False;Encrypt=True;TrustServerCertificate=true"
 
-Write-Host "Configuring Connection String"
-dotnet user-secrets init --project $project
-dotnet user-secrets set --project $project "ConnectionStrings:$database" "$connectionString" > $null
+try {
+    Write-Host "Configuring Connection String"
+    dotnet user-secrets init --project $project
+    dotnet user-secrets set "ConnectionStrings:$database" "$connectionString" --project $project > $null
 
-Write-Host "Starting SQL Server"
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$sqlPass" -p 1433:1433 --name $database -d mcr.microsoft.com/mssql/server:2019-latest
-dotnet ef migrations add InitialMigration -p $entities -s $project
+    Write-Host "Starting SQL Server"
+    docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$sqlPass" -p 1433:1433 --name $database -d mcr.microsoft.com/mssql/server:2019-latest
+    Start-Sleep -Seconds 2
+    dotnet ef database update --project $project
 
-Write-Host "Starting ProjectBank"
-dotnet run --project $project
-
-Write-Host "Stopping SQL Server"
-docker stop $DATABASE
-docker rm $DATABASE
-Remove-Item "$entities/Migrations" -Recurse
+    Write-Host "Starting ProjectBank"
+    dotnet run --project $project
+} finally {
+    Write-Host "Stopping SQL Server"
+    docker stop $database
+    docker rm $database
+}
--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 PROJECT="Server"
 ENTITIES="Server.Entities"
 DATABASE="sql_server"
 SQL_PASS=$(uuidgen)
-CONNECTION_STRING="Server=localhost;Database=$DATABASE;User Id=sa;Password=$SQL_PASS;Trusted_Connection=False;Encrypt=False"
+CONNECTION_STRING="Server=localhost;Database=$DATABASE;User Id=sa;Password=$SQL_PASS;Trusted_Connection=False;Encrypt=True;TrustServerCertificate=true"
 
 echo "Configuring Connection String"
 dotnet user-secrets init --project $PROJECT
-dotnet user-secrets set --project $PROJECT "ConnectionStrings:$DATABASE" "$CONNECTION_STRING" > /dev/null
+dotnet user-secrets set "ConnectionStrings:$DATABASE" "$CONNECTION_STRING" --project $PROJECT > /dev/null
 
 echo "Starting SQL Server"
 docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SQL_PASS" -p 1433:1433 --name $DATABASE -d mcr.microsoft.com/mssql/server:2019-latest
-dotnet ef migrations add InitialMigration -p $ENTITIES -s $PROJECT
+sleep 2
+dotnet ef database update --project $PROJECT
 
 echo "Starting ProjectBank (press Ctrl+C to stop)"
 dotnet run --project $PROJECT
@@ -20,4 +21,3 @@ dotnet run --project $PROJECT
 echo "Stopping SQL Server"
 docker stop $DATABASE
 docker rm $DATABASE
-rm -rf $ENTITIES/Migrations

06f1d9b62ab9cf885f0cdea1316b0941ce3da247
salsitu <sals@itu.dk>
2021-12-09 21:24:38 +0100

    Fix scripts.

--- run.ps1
+++ run.ps1
@@ -2,20 +2,19 @@ $project = "./Server"
 $entities = "./Server.Entities"
 $database = "sql_server"
 $sqlPass = New-Guid
-$connectionString = "Server=localhost;Database=$database;User Id=sa;Password=$sqlPass;Trusted_Connection=False;Encrypt=False"
+$connectionString = "Server=localhost;Database=$database;User Id=sa;Password=$sqlPass;Trusted_Connection=False;Encrypt=True;TrustServerCertificate=true"
 
 Write-Host "Configuring Connection String"
 dotnet user-secrets init --project $project
-dotnet user-secrets set --project $project "ConnectionStrings:$database" "$connectionString" > $null
+dotnet user-secrets set "ConnectionStrings:$database" "$connectionString" --project $project > $null
 
 Write-Host "Starting SQL Server"
 docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$sqlPass" -p 1433:1433 --name $database -d mcr.microsoft.com/mssql/server:2019-latest
-dotnet ef migrations add InitialMigration -p $entities -s $project
+dotnet ef database update --project $project
 
 Write-Host "Starting ProjectBank"
 dotnet run --project $project
 
 Write-Host "Stopping SQL Server"
-docker stop $DATABASE
-docker rm $DATABASE
-Remove-Item "$entities/Migrations" -Recurse
+docker stop $database
+docker rm $database
